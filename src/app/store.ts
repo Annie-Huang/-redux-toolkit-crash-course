e62a45f9ce37ea58716eea78b1cd703c4b0245d8
Annie Huang <annie.hn@gmail.com>
2022-01-30 21:47:20 +1100

    Create a boilerplate for reservationSlice.ts and add it to the store reducer object.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import reservationsReducer from '../features/reservationSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    reservations: reservationsReducer,
+  },
 });
 
 // https://redux-toolkit.js.org/tutorials/quick-start#create-a-redux-store
