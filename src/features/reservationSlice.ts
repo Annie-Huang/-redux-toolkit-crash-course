0123b69a4cae0364f3c7bfd9de7cd2428138252b
Annie Huang <annie.hn@gmail.com>
2022-01-30 21:58:35 +1100

    Create "interface ReservationState" to restrict the intialState. Add 'addReservation' reducer

--- src/features/reservationSlice.ts
+++ src/features/reservationSlice.ts
@@ -1,6 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-const initialState = {
+interface ReservationState {
+  value: string[];
+}
+
+const initialState: ReservationState = {
   value: [],
 };
 
@@ -8,7 +12,9 @@ export const reservationSlice = createSlice({
   name: 'reservations',
   initialState,
   reducers: {
-    // add: (state, payload) => {},
+    addReservation: (state, action: PayloadAction<string>) => {
+      state.value.push(action.payload);
+    },
   },
 });
 
