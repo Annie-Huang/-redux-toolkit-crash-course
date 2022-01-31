8aff6803a0759b5260b6f8810fd9f76a0c737081
Annie Huang <annie.hn@gmail.com>
2022-01-31 18:38:07 +1100

    Create a 'addCustomer' reducer in customerSlice.ts and export action

--- src/features/customerSlice.ts
+++ src/features/customerSlice.ts
@@ -17,9 +17,13 @@ const initialState: CustomerState = {
 export const customerSlice = createSlice({
   name: 'customer',
   initialState,
-  reducers: {},
+  reducers: {
+    addCustomer: (state, action: PayloadAction<Customer>) => {
+      state.value.push(action.payload);
+    },
+  },
 });
 
-export const {} = customerSlice.actions;
+export const { addCustomer } = customerSlice.actions;
 
 export default customerSlice.reducer;
